05cff749e8553955a91ace8e80c7d08b1120aa93
oscars.skittles@gmail.com <oscars-skittles>
2025-09-11 12:53:53 +0800

    Remove unused CSS import from test1.vue to streamline component definition.

--- src/views/test1.vue
+++ src/views/test1.vue
@@ -4,12 +4,10 @@
 <script lang="ts" setup>
 import test from './test.vue';
 import { defineCustomElement, h } from 'vue';
-import styleCss from '../../core/dist/index.min.css';
 const ExampleElement = defineCustomElement({
   setup() {
     return () => h(test);
   },
-  styles: [styleCss],
 });
 customElements.define('my-example', ExampleElement);
 </script>
